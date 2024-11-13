bff40c205c4a982b963d2b66fe5f41ef4f687b14
n1noT <n8no.ttn@gmail.com>
2024-11-13 11:22:06 +0100

    TP4 ended

--- html/index.php
+++ html/index.php
@@ -9,51 +9,93 @@ function getLoadTime () {
     return $time;
 }
 
+function dd($var) {
+    var_dump($var);
+    die();
+}
+
 $mysqli = getMySQLiConnection();
 
-$data = $mysqli->query("SELECT * FROM `users`");
+// On détermine sur quelle page on se trouve
+if(isset($_GET['page']) && !empty($_GET['page'])){
+    $currentPage = (int) strip_tags($_GET['page']) ;
+}else{
+    $currentPage = 1;
+}
+
+$resultCount = $mysqli->query("SELECT COUNT(*) as count FROM `users`")->fetch_assoc();
+$userPerPage = 16;
+$nbPage = ceil($resultCount['count'] / $userPerPage);
+
+
+$data = $mysqli->query("SELECT `id_users`, `type`, `adresse`, `nom`, `email` FROM `users` LIMIT $userPerPage OFFSET " . ($currentPage - 1) * $userPerPage)->fetch_all(MYSQLI_ASSOC);
 
 if($mysqli->connect_error){
     die('Erreur : ' .$mysqli->connect_error);
 }
+
+
 
 ?>
 
 <html>
     <head>
         <title>USER CRUD</title>
+        <link rel="stylesheet" href="style.css">
     </head>
     <body>
-        <h1>Créer un utilisateur</h1>
-        <form action="createUser.php" method="post">
-            <label for="type">Type</label>
-            <input type="text" name="type" id="type">
-            <label for="adresse">Adresse</label>
-            <input type="text" name="adresse" id="adresse">
-            <label for="nom">Nom</label>
-            <input type="text" name="nom" id="nom">
-            <label for="email">Email</label>
-            <input type="text" name="email" id="email">
-            <input type="submit" value="Créer">
-        </form>
-        <h1>Supprimer un utilisateur</h1>
-        <form action="deleteUser.php" method="post">
-            <label for="id">ID</label>
-            <input type="text" name="id" id="id">
-            <input type="submit" value="Supprimer">
-        </form>
-
-        <h1>Liste des utilisateurs</h1>
+        <nav class="nav">
+            <h1>Liste des utilisateurs</h1>
+            <a href="createUser.php">
+                <button class="button">
+                    + Créer un utilisateur
+                </button>
+            </a>
+        </nav>
+        <?php
+        if(isset($_GET['message'])) {
+            echo '<p class="message">' . htmlspecialchars($_GET['message']) . '</p>';
+        }
+        ?>
+        <ul class="pagination">
+            <li class="pagination__elt">
+                <a href="index.php?page=1">Premiere page</a>
+            </li>
+                 <?php
+                    if($currentPage > 1){
+                        echo '<li class="pagination__elt"><a href="index.php?page=' . ($currentPage - 1) . '">Précédente</a></li>';
+                    }
+                ?>
+            <li class="pagination__elt">
+                <p><?php echo $currentPage ?></p>
+            </li>
+            <?php
+                if($currentPage < $nbPage){
+                    echo '<li class="pagination__elt"><a href="index.php?page=' . ($currentPage + 1) . '">Suivante</a></li>';
+                }
+            ?>
+            <li class="pagination__elt">
+                <a href="index.php?page=<?= $nbPage ?>">Dernière page</a>
+            </li>
+            
+        </ul>
+        <ul class='list'>
         <?php
             foreach ($data as $row) {
-                echo "<p>
-                 ID :  " . $row['id_users'] . "<br>
-                 Type : " .  $row['type'] . "<br>
-                 Adresse : " . $row['adresse'] . "<br>
-                 Nom : " . $row['nom'] . "<br>
-                 Email : " .  $row['email'] . "<br>
-                 </p>";
+                echo "<li class='list__elt'><p>
+                 ID :  " . htmlspecialchars($row['id_users']) . "<br>
+                   Adresse : " . htmlspecialchars($row['adresse']) . "<br>
+
+                 Nom : " . htmlspecialchars($row['nom']) . "<br>
+                    Email : " . htmlspecialchars($row['email']) . "<br>
+                     </p>
+                    <div class='actions'>
+                    <a href='deleteUser.php?id=" .  htmlspecialchars($row['id_users']) . "'><button class='button button--del'>Supprimer</button></a>
+                    <a href='createUser.php?id=" .  htmlspecialchars($row['id_users']) . "'><button class='button button--outline'>Modifier</button></a>
+                    </div>
+                 </li>";
             }
         ?>
+        </ul>
     </body>
 </html>
